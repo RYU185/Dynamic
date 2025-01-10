c22432cc8d556ff0b893c7eb3f6f986d0309ddda
RYU185 <fence365@naver.com>
2025-01-10 14:52:10 +0900

    Update Board.java

--- dynamic/src/main/java/com/dw/dynamic/model/Board.java
+++ dynamic/src/main/java/com/dw/dynamic/model/Board.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 @ToString
 @Entity
-@Table(name = "Board")
+@Table(name = "board")
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
