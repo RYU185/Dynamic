9b36c2dee3d04f7afc601fe6151d8f59ddf4ff41
pepe <dltmdqls15@gmail.com>
2025-01-16 18:15:18 +0900

    .

--- dynamic/src/main/java/com/dw/dynamic/controller/PayrollSubscriptionController.java
+++ dynamic/src/main/java/com/dw/dynamic/controller/PayrollSubscriptionController.java
@@ -1,7 +1,37 @@
 package com.dw.dynamic.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.dw.dynamic.DTO.PayrollSubscriptionsEnrollmentAndIncomeDTO;
+import com.dw.dynamic.service.PayrollSubscriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/payrollsubscription")
 public class PayrollSubscriptionController {
+    @Autowired
+    PayrollSubscriptionService payrollSubscriptionService;
+
+    @GetMapping("/all")
+    public ResponseEntity<PayrollSubscriptionsEnrollmentAndIncomeDTO> getAllPayrollSubscriptions() {
+        return new ResponseEntity<>(
+                payrollSubscriptionService.getAllPayrollSubscriptions(),
+                HttpStatus.OK);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<PayrollSubscriptionsEnrollmentAndIncomeDTO> savePayrollSubscription(@RequestBody PayrollSubscriptionsEnrollmentAndIncomeDTO payrollSubscriptionsEnrollmentAndIncomeDTO) {
+        return new ResponseEntity<>(
+                payrollSubscriptionService.savePayrollSubscription(payrollSubscriptionsEnrollmentAndIncomeDTO),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<PayrollSubscriptionsEnrollmentAndIncomeDTO> getPayrollSubscriptionById(@PathVariable Long id) {
+        return new ResponseEntity<>(
+                payrollSubscriptionService.getPayrollSubscriptionById(id),
+                HttpStatus.OK);
+    }
 }
+
