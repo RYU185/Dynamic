4ca255e4ba686a4096ed1839ee7d8a11890d86a5
RYU185 <fence365@naver.com>
2025-01-16 16:44:38 +0900

    .

--- dynamic/src/main/java/com/dw/dynamic/controller/ProductController.java
+++ dynamic/src/main/java/com/dw/dynamic/controller/ProductController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,8 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+
+    @GetMapping("payrollsubscription/enrollments-and-income")
     public ResponseEntity<List<PayrollSubscriptionsEnrollmentAndIncomeDTO>> etPayrollSubscriptionsEnrollmentsAndIncomes(HttpServletRequest request){
         return new ResponseEntity<>(
                 productService.getPayrollSubscriptionsEnrollmentsAndIncomes(request),
@@ -26,12 +29,15 @@ public class ProductController {
         );
     }
 
+    @GetMapping("category/enrollments-and-income")
     public ResponseEntity<List<CategoryEnrollmentAndIncomeDTO>> getCategoryEnrollmentsAndIncomes(HttpServletRequest request){
         return new ResponseEntity<>(
                 productService.getCategoryEnrollmentsAndIncomes(request),
                 HttpStatus.OK
         );
     }
+
+    @GetMapping("courses/enrollments-and-income")
     public ResponseEntity<List<CourseEnrollmentAndIncomeDTO>> getCoursesEnrollmentsAndIncomes(HttpServletRequest request){
         return new ResponseEntity<>(
                 productService.getCoursesEnrollmentsAndIncomes(request),
