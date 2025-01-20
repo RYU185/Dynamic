0fb7b1d26bf99738fdb9925252b6ce28a0db3702
RYU185 <fence365@naver.com>
2025-01-20 18:50:55 +0900

    ..

--- dynamic/src/main/java/com/dw/dynamic/repository/PurchaseHistoryRepository.java
+++ dynamic/src/main/java/com/dw/dynamic/repository/PurchaseHistoryRepository.java
@@ -1,11 +1,24 @@
 package com.dw.dynamic.repository;
 
 import com.dw.dynamic.model.PurchaseHistory;
+import com.dw.dynamic.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PurchaseHistoryRepository extends JpaRepository<PurchaseHistory,Long> {
 
-    public  PurchaseHistory findByProductId(String productId);
+    PurchaseHistory findByProductId(String productId);
+
+    @Query("select c from Course c where c.title like %:title%")
+    List<PurchaseHistory> findCourseByTitleLike(String title);
+
+    @Query("select ps from PayrollSubscription ps where ps.title like %:title%")
+    List<PurchaseHistory> findPayrollSubscriptionByTitleLike(String title);
+
+    List<PurchaseHistory> findByUser(User currentUser);
+
+
+
 }
