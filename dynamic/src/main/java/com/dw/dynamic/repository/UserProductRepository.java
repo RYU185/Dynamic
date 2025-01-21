be8af9fb88c80094e05281f7767474e1c7d65af1
RYU185 <fence365@naver.com>
2025-01-21 11:19:48 +0900

    123

--- dynamic/src/main/java/com/dw/dynamic/repository/UserProductRepository.java
+++ dynamic/src/main/java/com/dw/dynamic/repository/UserProductRepository.java
@@ -1,10 +1,25 @@
 package com.dw.dynamic.repository;
 
+import com.dw.dynamic.model.User;
 import com.dw.dynamic.model.UserProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserProductRepository extends JpaRepository<UserProduct,Long> {
-   // List<UserProduct> findByProductIdLike(String productName);
+    List<UserProduct> findByUser(User currentUser);
+
+    @Query("select up from UserProduct up " +
+            "where up.user = :currentUser " +
+            "and up.product.id = :productId")
+    List<UserProduct> findByProductId(User currentUser, String productId);
+
+    @Query("select up from UserProduct up " +
+            "join up.product p " +
+            "left join Course c on p.id = c.id " +
+            "left join PayrollSubscription ps on p.id = ps.id " +
+            "where up.user = :currentUser " +
+            "and (c.title like %:productName% or ps.title like %:productName%)")
+    List<UserProduct> findByProductNameLike(User currentUser, String productName);
 }
--- dynamic/src/main/java/com/dw/dynamic/service/UserProductService.java
+++ dynamic/src/main/java/com/dw/dynamic/service/UserProductService.java
@@ -1,11 +1,14 @@
 package com.dw.dynamic.service;
 
 import com.dw.dynamic.DTO.UserProductDTO;
+import com.dw.dynamic.exception.InvalidRequestException;
 import com.dw.dynamic.exception.ResourceNotFoundException;
+import com.dw.dynamic.model.User;
 import com.dw.dynamic.model.UserProduct;
 import com.dw.dynamic.repository.PurchaseHistoryRepository;
 import com.dw.dynamic.repository.UserProductRepository;
 import com.dw.dynamic.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,20 +19,58 @@ public class UserProductService {
     @Autowired
     UserProductRepository userProductRepository;
     @Autowired
-    UserRepository userRepository;
-    @Autowired
-    PurchaseHistoryRepository purchaseHistoryRepository;
+    UserService userService;
 
-    public List<UserProductDTO> getAllUserProducts (){
-        return userProductRepository.findAll().stream().map(UserProduct::toDTO).toList();
+
+    public List<UserProductDTO> getAllUserProducts (HttpServletRequest request){
+        User currentUser = userService.getCurrentUser(request);
+        if (currentUser == null){
+            throw new IllegalArgumentException("올바르지 않은 접근입니다");
+        }
+        return userProductRepository.findByUser(currentUser).stream().map(UserProduct::toDTO).toList();
+
     }
 
-    public UserProductDTO getUserProductById(Long id){
+    public UserProductDTO getUserProductById(Long id, HttpServletRequest request) {
+        User currentUser = userService.getCurrentUser(request);
+        if (currentUser == null) {
+            throw new IllegalArgumentException("올바르지 않은 접근입니다.");
+        }
+        UserProduct userProduct = userProductRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("올바르지 않은 접근입니다."));
+
+        if (!userProduct.getUser().equals(currentUser)) {
+            throw new InvalidRequestException("정상적인 요청이 아닙니다");
+        }
+
         return userProductRepository.findById(id).map(UserProduct::toDTO)
-                .orElseThrow(()-> new ResourceNotFoundException("ID가 없습니다"));
+                .orElseThrow(()-> new ResourceNotFoundException("존재하는 ID가 없습니다"));
     }
 
-//    public List<UserProductDTO> getUserProductByProductName(String productId){
-//        return userProductRepository.findByProductIdLike(productId).stream().map(UserProduct::toDTO).toList();
-//    }
+    public List<UserProductDTO> getUserProductByProductId(String productId, HttpServletRequest request){
+        User currentUser = userService.getCurrentUser(request);
+        if (currentUser == null){
+            throw new IllegalArgumentException("올바르지 않은 접근입니다");
+        }
+        List<UserProduct> userProduct = userProductRepository.findByProductId(currentUser,productId).stream().toList();
+
+
+        if (userProduct.isEmpty()){
+            throw new IllegalArgumentException("해당 제품을 찾을 수 없습니다");
+        }
+
+        List<UserProduct> userProducts = userProductRepository.findByProductId(currentUser, productId);
+        return userProducts.stream().map(UserProduct::toDTO).toList();
+    }
+
+    public List<UserProductDTO> getUserProductByProductName(String productName, HttpServletRequest request){
+        User currentUser = userService.getCurrentUser(request);
+        if (currentUser == null){
+            throw new IllegalArgumentException("올바르지 않은 접근입니다");
+        }
+
+        List<UserProduct> userProducts = userProductRepository.findByProductNameLike(currentUser, productName);
+
+        return userProducts.stream().map(UserProduct::toDTO).toList();
+    }
 }
