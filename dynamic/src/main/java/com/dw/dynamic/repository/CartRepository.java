535e6cac167284e7ff2e44d5cb3d6ed53274702b
NBear32 <hansel32153@gmail.com>
2025-02-18 12:50:45 +0900

    z

--- dynamic/src/main/java/com/dw/dynamic/repository/CartRepository.java
+++ dynamic/src/main/java/com/dw/dynamic/repository/CartRepository.java
@@ -3,6 +3,7 @@ package com.dw.dynamic.repository;
 import com.dw.dynamic.model.Cart;
 import com.dw.dynamic.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,5 +12,6 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
 
     List<Cart> findByProductId(String id);
 
+    @Query("select c from Cart c where c.isActive=true")
     List<Cart> findByIsActive();
 }
