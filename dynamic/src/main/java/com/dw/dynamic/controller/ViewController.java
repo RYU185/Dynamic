75b20c0496046755be0386a797d0ec8f0fa09a4c
NBear32 <hansel32153@gmail.com>
2025-02-18 17:43:54 +0900

    .

---
+++ dynamic/src/main/java/com/dw/dynamic/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.dw.dynamic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/dynamic/index.html")
+    public String index() {
+        return "forward:/index.html";
+    }
+}
