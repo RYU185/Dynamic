d1fb132faed68a178a70dd0b679234e9205eae01
NBear32 <hansel32153@gmail.com>
2025-02-19 20:10:09 +0900

    .

--- dynamic/src/main/java/com/dw/dynamic/controller/ViewController.java
+++ dynamic/src/main/java/com/dw/dynamic/controller/ViewController.java
@@ -21,6 +21,9 @@ public class ViewController {
         return "forward:/businessoperator_true.html";
     }
 
+    @GetMapping("/dynamic/businessoperator_false.html")
+    public String businessoperatbusinessoperator_false() {return "forward:/businessoperator_false.html";}
+
     @GetMapping("/dynamic/employee_information.html")
     public String employee_information() {
         return "forward:/employee_information.html";
@@ -37,9 +40,7 @@ public class ViewController {
     }
 
     @GetMapping("/dynamic/myCourse.html")
-    public String myCourse() {
-        return "forward:/myCourse.html";
-    }
+    public String myCourse() {return "forward:/myCourse.html";}
 
     @GetMapping("/dynamic/mypage.html")
     public String mypage() {
@@ -52,8 +53,54 @@ public class ViewController {
     }
 
     @GetMapping("/dynamic/regural_part_time_employee.html")
-    public String regural_part_time_employee() {
-        return "forward:/regural_part_time_employee.html";
-    }
+    public String regural_part_time_employee() {return "forward:/regural_part_time_employee.html";}
+
+    @GetMapping("/dynamic/add_employee.html")
+    public String add_employee() {return "forward:/add_employee.html";}
+
+    @GetMapping("/dynamic/add_part_time.html")
+    public String add_part_time() {return "forward:/add_part_time.html";}
+
+    @GetMapping("/dynamic/add_regural_part_time.html")
+    public String add_regural_part_time() {return "forward:/add_regural_part_time.html";}
+
+    @GetMapping("/dynamic/board.html")
+    public String board() {return "forward:/board.html";}
+
+    @GetMapping("/dynamic/cart.html")
+    public String cart() {return "forward:/cart.html";}
+
+    @GetMapping("/dynamic/edit_profile.html")
+    public String edit_profile() {return "forward:/edit_profile.html";}
+
+    @GetMapping("/dynamic/find_id.html")
+    public String find_id() {return "forward:/find_id.html";}
+
+    @GetMapping("/dynamic/formation_data.html")
+    public String formation_data() {return "forward:/formation_data.html";}
+
+    @GetMapping("/dynamic/guide_detail.html")
+    public String guide_detail() {return "forward:/guide_detail.html";}
+
+    @GetMapping("/dynamic/main_guide.html")
+    public String main_guide() {return "forward:/main_guide.html";}
+
+    @GetMapping("/dynamic/main_notice.html")
+    public String main_notice() {return "forward:/main_notice.html";}
+
+    @GetMapping("/dynamic/my_payrolltemplate.html")
+    public String my_payrolltemplate() {return "forward:/my_payrolltemplate.html";}
+
+    @GetMapping("/dynamic/my_point.html")
+    public String my_point() {return "forward:/my_point.html";}
+
+    @GetMapping("/dynamic/notice_detail.html")
+    public String notice_detail() {return "forward:/notice_detail.html";}
+
+    @GetMapping("/dynamic/product.html")
+    public String product() {return "forward:/product.html";}
+
+
+
 
 }
