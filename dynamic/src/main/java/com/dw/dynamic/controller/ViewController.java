4a531fe11669c25d586586d36d66464773ad2942
NBear32 <hansel32153@gmail.com>
2025-02-19 19:03:41 +0900

    .

--- dynamic/src/main/java/com/dw/dynamic/controller/ViewController.java
+++ dynamic/src/main/java/com/dw/dynamic/controller/ViewController.java
@@ -11,9 +11,49 @@ public class ViewController {
         return "forward:/index.html";
     }
 
-
     @GetMapping("/dynamic/myPurchaseHistory.html")
     public String myPurchaseHistory() {
         return "forward:/myPurchaseHistory.html";
     }
+
+    @GetMapping("/dynamic/businessoperator_1.html")
+    public String businessoperator_1() {
+        return "forward:/businessoperator_1.html";
+    }
+
+    @GetMapping("/dynamic/employee_information.html")
+    public String employee_information() {
+        return "forward:/employee_information.html";
+    }
+
+    @GetMapping("/dynamic/employee_modify.html")
+    public String employee_modify() {
+        return "forward:/employee_modify.html";
+    }
+
+    @GetMapping("/dynamic/joinMembership.html")
+    public String joinMembership() {
+        return "forward:/joinMembership.html";
+    }
+
+    @GetMapping("/dynamic/myCourse.html")
+    public String myCourse() {
+        return "forward:/myCourse.html";
+    }
+
+    @GetMapping("/dynamic/mypage.html")
+    public String mypage() {
+        return "forward:/mypage.html";
+    }
+
+    @GetMapping("/dynamic/part_time.html")
+    public String part_time() {
+        return "forward:/part_time.html";
+    }
+
+    @GetMapping("/dynamic/regural_part_time_employee.html")
+    public String regural_part_time_employee() {
+        return "forward:/regural_part_time_employee.html";
+    }
+
 }
