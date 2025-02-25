73dede48188bfc48f5f4c9b54f7737377e0f8ec0
RYU185 <fence365@naver.com>
2025-02-25 13:36:39 +0900

    11

--- dynamic/src/main/resources/static/js/board.js
+++ dynamic/src/main/resources/static/js/board.js
@@ -1,10 +1,12 @@
 
+const closeBtn = document.querySelector('.closeBtn img[src="./img/X.png"]');
 const opacity = document.querySelector('.opacity');
 const overlay = document.querySelector('.overlay');
 const cells = document.querySelectorAll("table tr:not(:first-child) td:nth-child(2)");
-const content1 = document.querySelector('.pop-up1');
-const content2 = document.querySelector('.pop-up2');
+const content1 = document.querySelector('.pop-up1'); // 본문등록
+const content2 = document.querySelector('.pop-up2'); // 댓글등록
 const addNewPost = document.querySelector('.btn img[src="./img/add.png"]');
+const NewPostSubmit = document.querySelector('.')
 
 function openModalNewPost(){
   content1.style.display = "block";
@@ -32,6 +34,8 @@ cells.forEach(cell => {
 });
 
 opacity.addEventListener('click', closeModal);
+
+closeBtn.addEventListener('click', closeModal);
 
 //  내가 누를 요소(html에서 가져올 것) + 기능(이걸 누르면 css에서 어떻게 변하는지)
 
