f9594f8d070a3ddbf1e44bfcb82d9ad26080c8a3
RYU185 <fence365@naver.com>
2025-02-25 12:18:21 +0900

    123

--- dynamic/src/main/resources/static/js/boardModal.js
+++ dynamic/src/main/resources/static/js/boardModal.js
@@ -1,24 +1,36 @@
 
 const opacity = document.querySelector('.opacity');
-const overlay = document.querySelector('.overlay')
+const overlay = document.querySelector('.overlay');
 const cells = document.querySelectorAll("table tr:not(:first-child) td:nth-child(2)");
 const content1 = document.querySelector('.pop-up1');
 const content2 = document.querySelector('.pop-up2');
+const addNewPost = document.querySelector('.btn img[src="./img/add.png"]');
 
-function openModal() {
+function openModalNewPost(){
+  content1.style.display = "block";
+  opacity.style.display = "block";
+  overlay.style.display = "block";
+}
+
+function openModalDetail() {
   content2.style.display = "block";
   opacity.style.display = "block";
   overlay.style.display = "block";
 }
 
 function closeModal() {
+  content1.style.display = "none"
   content2.style.display = "none";
   opacity.style.display = "none";
   overlay.style.display = "none";
 }
 
+addNewPost.addEventListener('click', openModalNewPost);
+
 cells.forEach(cell => {
-  cell.addEventListener("click", openModal);
+  cell.addEventListener("click", openModalDetail);
 });
 
 opacity.addEventListener('click', closeModal);
+
+//  내가 누를 요소(html에서 가져올 것) + 기능(이걸 누르면 css에서 어떻게 변하는지)
