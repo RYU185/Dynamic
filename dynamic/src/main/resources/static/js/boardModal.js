6072fddd2a9269f307acf62140449585304c2745
RYU185 <fence365@naver.com>
2025-02-25 11:17:10 +0900

    Update boardModal.js

--- dynamic/src/main/resources/static/js/boardModal.js
+++ dynamic/src/main/resources/static/js/boardModal.js
@@ -1,22 +1,32 @@
 
 const opacity = document.querySelector('.opacity');
+const overlay = document.querySelector('.overlay')
+const cells = document.querySelectorAll("table tr:not(:first-child) td:nth-child(2)");
 const content1 = document.querySelector('.pop-up1');
-const content2 = document.querySelector(
-  '.main_box tr:not(:first-of-type) td:nth-of-type(2)');
+const content2 = document.querySelector('.pop-up2');
 
-content2.addEventListener('click', function(event){
+// 바깥 클릭시 display 속성 바꾸기
+content2.addEventListener('click', function(){
   if (content2.style.display === 'none'){
       content2.style.display = 'block';
       opacity.style.display = 'block';
+      overlay.style.display = 'block';
   }
   else {
     content2.style.display = 'none';
   }
 });
 
-opacity.addEventListener('click', function(event){
-  if(!content2.contains(event.target)){
+cells.forEach(cell => {
+  cell.addEventListener("click", function() {
+      content2.style.display = "block";
+      opacity.style.display = "block";
+      overlay.style.display = 'block';
+  });
+});
+
+overlay.addEventListener('click', function(){
     content2.style.display = 'none';
     opacity.style.display = 'none'
-  }
+    overlay.style.display = 'none';
 })
