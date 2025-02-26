04ef5143847450d45d63e75a3e87a1d5256d09ee
RYU185 <fence365@naver.com>
2025-02-26 19:21:24 +0900

    Update product.js

--- dynamic/src/main/resources/static/js/product.js
+++ dynamic/src/main/resources/static/js/product.js
@@ -73,3 +73,43 @@ $(document).ready(function () {
 
     })
 });
+
+
+$(document).ready(function(){
+    $.ajax({
+        url: '/api/product/all',
+        method:'get',
+        contentType:'application/json',
+        success: function (response){
+            $('article').empty();
+            response.forEach((element) => {
+                var $article = $(`
+                    <article>
+                        <h2>제품카테고리: ${element.category}</h2>
+                        <div>
+                            <span>가격: </span><span class="price">${
+                                element.price
+                            }</span><span>원</span>
+                        </div>
+                        <div>
+                            <span>상태: </span><span class="status">${
+                                element.isActive ? "판매 중" : "판매 종료"
+                            }</span>
+                        </div>
+                        <div class="btn">
+                            <button class="btnInCart" data-id="${
+                                element.id
+                            }">장바구니</button>
+                            <button class="btnPurchase">구매</button>
+                        </div>
+                    </article>
+            `);
+
+            $('article').append($article);
+            $article.on('click',() => {
+                console.log(element.category + ' '+ element.title);
+                window.location.href = '/'
+            })
+        })
+    }})
+})
