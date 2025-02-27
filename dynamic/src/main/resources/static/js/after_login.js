23e8dda93a902789f8d4a78fbbfed802d500e4e4
NBear32 <hansel32153@gmail.com>
2025-02-27 11:55:41 +0900

    .

--- dynamic/src/main/resources/static/js/after_login.js
+++ dynamic/src/main/resources/static/js/after_login.js
@@ -1,5 +1,5 @@
 const userCopy = JSON.parse(sessionStorage.getItem("userName"));
-$(".name").text(userCopy);
+
 
 // 버튼과 콘텐츠 요소 가져오기
 const button = document.querySelector('.button');
@@ -37,3 +37,16 @@ document.querySelector('.log_out').addEventListener('click', function () {
     sessionStorage.removeItem("userName");
     window.location.href = '/index.html';
 })
+
+
+
+
+window.onload = function () {
+    if (!userCopy) {
+        alert('로그인 후 이용 가능합니다.');
+        window.location.href = '/login.html';
+    }
+    else {
+        $('.name').text(userCopy);
+    }
+};
