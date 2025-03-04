14fbaf962dae18d629086b21a80146cb52a1ad32
NBear32 <hansel32153@gmail.com>
2025-03-04 20:18:03 +0900

    .

--- dynamic/src/main/resources/static/js/my_page.js
+++ dynamic/src/main/resources/static/js/my_page.js
@@ -1,2 +1,12 @@
+const existBusinessOperator = sessionStorage.getItem('existBusinessOperator');
+console.log(existBusinessOperator);
+$(document).on('click', '#employee_box', function () {
 
-$(document)
+    const isexist = existBusinessOperator === 'true';
+    if (!isexist) {
+        alert('사업자가 없을 경우 별도로 직원 등록 및 조회가 불가능합니다');
+        return;
+    } else {
+        window.location.href = '/my_employee.html';
+    }
+})
