7081f77c995440e5f8dbf55c74a95e35dd80a4ad
NBear32 <hansel32153@gmail.com>
2025-03-06 17:21:21 +0900

    /

--- dynamic/src/main/resources/static/js/joinMembership.js
+++ dynamic/src/main/resources/static/js/joinMembership.js
@@ -4,5 +4,5 @@ $('#true').on('click', function () {
   window.location.href = '/businessoperator_true.html';
 })
 $('#false').on('click', function () {
-  window.location.href = '/businessoperator_true.html';
+  window.location.href = '/businessoperator_false.html';
 })
